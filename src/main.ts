7951ce496f51368c1e71dbd25e5d38d28d90b01c
Scott Nesham <sanesham@gmail.com>
2021-11-15 08:35:47 -0600

    HMR

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
