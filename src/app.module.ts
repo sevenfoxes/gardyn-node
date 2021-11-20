944a62ce4550a76a8568cfd855dcfbb056bf9f7b
Scott Nesham <sanesham@gmail.com>
2021-11-20 14:14:24 -0600

    sdf

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GardensController } from './gardens/gardens.controller';
 import { GardensService } from './gardens/gardens.service';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ConfigService.getTypeOrmConfig())],
   controllers: [AppController, GardensController],
-  providers: [AppService, GardensService],
+  providers: [AppService, GardensService, ConfigService],
 })
 export class AppModule {}
