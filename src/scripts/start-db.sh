b3578d00011910cc041d741a46230d42ccaec881
Scott Nesham <sanesham@gmail.com>
2021-11-21 08:10:58 -0600

    integrate typeorm and postgres

--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
+export $(egrep -v '^#' .env | xargs)
 
-SERVER="gardyn";
-PW="admin";
-DB="my_database";
 
-echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
-(docker kill $SERVER || :) && \
-  (docker rm $SERVER || :) && \
-  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
-  -e PGPASSWORD=$PW \
-  -p 5432:5432 \
+printenv | grep POSTGRES;
+
+echo "echo stop & remove old docker [$POSTGRES_SERVER] and starting new fresh instance of [$POSTGRES_SERVER]"
+(docker kill $POSTGRES_SERVER || :) && \
+  (docker rm $POSTGRES_SERVER || :) && \
+  docker run --name $POSTGRES_SERVER -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  -e PGPASSWORD=$POSTGRES_PASSWORD \
+  -p $POSTGRES_PORT:$POSTGRES_PORT \
   -d postgres
 
 # wait for pg to start
-echo "sleep wait for pg-server [$SERVER] to start";
+echo "sleep wait for pg-server [$POSTGRES_SERVER] to start";
 sleep 3;
 
 # create the db
-echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
-echo "\l" | docker exec -i $SERVER psql -U postgres
+echo "CREATE DATABASE $POSTGRES_DATABASE ENCODING 'UTF-8';" | docker exec -i $POSTGRES_SERVER psql -U postgres
+echo "\l" | docker exec -i $POSTGRES_SERVER psql -U postgres
