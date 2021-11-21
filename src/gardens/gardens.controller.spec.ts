17765eb43e1e7ee1ffb1b4d186d487172926d6a1
Scott Nesham <sanesham@gmail.com>
2021-11-21 14:04:39 -0600

    get the stub tests passing again

--- src/gardens/gardens.controller.spec.ts
+++ src/gardens/gardens.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GardensController } from './gardens.controller';
+import { GardensService } from './gardens.service';
 
 describe('GardensController', () => {
+  let app: TestingModule;
   let controller: GardensController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GardensController],
+      providers: [GardensService],
     }).compile();
 
     controller = module.get<GardensController>(GardensController);
