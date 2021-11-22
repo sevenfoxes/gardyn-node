b58fbbddb94c6c26a7972d112e2989b0ae6d8dc4
Scott Nesham <sanesham@gmail.com>
2021-11-22 05:00:51 -0600

    make react compile

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import { App } from './App';
 
 const wrapper = document.getElementById('root');
-wrapper ? ReactDOM.render(React.createElement(App), wrapper) : false;
+
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
